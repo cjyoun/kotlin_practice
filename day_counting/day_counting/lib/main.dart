cbec265b4838eb6bcffedff873d7ce3170ab8a41
jychae <cjy@spsoft.co.kr>
2025-01-22 17:41:08 +0900

    테마 설정하고 적용하기

--- day_counting/day_counting/lib/main.dart
+++ day_counting/day_counting/lib/main.dart
@@ -4,6 +4,30 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'sunflower',    // 테마의 기본 글꼴 설정(따로 fontFamily를 설정하지 않으면 이 fontFamily를 바라봄)
+        textTheme: TextTheme(
+            displayLarge: TextStyle(
+              color: Colors.white,
+              fontSize: 80.0,
+              fontFamily: 'parisienne',
+            ),
+          displayMedium: TextStyle(
+            color: Colors.white,
+            fontSize: 50.0,
+            fontWeight: FontWeight.w700
+          ),
+          bodyLarge: TextStyle(
+            color: Colors.white,
+            fontSize: 30.0,
+            fontWeight: FontWeight.w500
+          ),
+          bodyMedium: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+          ),
+        ),
+      ),
       home: HomeScreen(),
     ),
   );
--- day_counting/day_counting/lib/screen/home_screen.dart
+++ day_counting/day_counting/lib/screen/home_screen.dart
@@ -1,11 +1,55 @@
 import 'package:flutter/material.dart';
 
-
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final textTheme = Theme.of(context).textTheme;
+
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('asset/img/image_4.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Column(
+              children: [
+                Text(
+                  'U & I',
+                  style: textTheme.displayLarge,
+                ),
+                Text(
+                  '우리 처음 만난 날',
+                  style:  textTheme.bodyLarge,
+                ),
+                Text(
+                  '2019.04.27',
+                  style:  textTheme.bodyMedium,
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.favorite, // 하트모양 아이콘
+                    color: Colors.red,
+                    size: 80.0,
+                  ),
+                ),
+                Text(
+                  'D+1',
+                  style:  textTheme.displayMedium,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
